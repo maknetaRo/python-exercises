4ee4095a6e92d5735f1981ad510fd38c4b4e1209
Magda <makneta@gmail.com>
2018-07-02 13:20:12 +0200

    Exercise 4

---
+++ definition/def4.py
@@ -0,0 +1,9 @@
+"""
+4. Write a Python program to reverse a string.
+Sample String : "1234abcd"
+Expected Output : "dcba4321"
+"""
+def reverse_string(strng):
+    return strng[::-1]
+
+print(reverse_string("1234abcd"))
